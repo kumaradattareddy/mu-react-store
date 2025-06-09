bff68fdbe74cd1e0aa7d7f620dcaef8ceefe11d1
kumara <kumaradattareddy@gmail.com>
2025-06-09 16:41:19 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './Header';
-import Footer from './Footer';
-import Home from './home';
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Login from "./Login";
+import Register from "./Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 function App() {
   return (
-      <>
-      <Header className = "mu_store"/>
-      <Home/>
-      <Footer/>
-    
-      </>
-  )
+    <div>
+      <BrowserRouter>
+        <Header name="mu-react-store" />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
-
-export default App
+export default App;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-
+import React from "react";
+import "./Home.css"
 export default function Home() {
+  const products = [
+    {
+      id: 1,
+      name: "Product 1",
+      desc: "This is the description of the product",
+      price: 45,
+      imgUrl: "https://picsum.photos/id/1/300/300",
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      desc: "This is the description of the product",
+      price: 50,
+      imgUrl: "https://picsum.photos/id/2/300/300",
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      desc: "This is the description of the product",
+      price: 75,
+      imgUrl: "https://picsum.photos/id/3/300/300",
+    },
+  ];
+
   return (
-    <div>
-        <h1>Hello World</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa pariatur debitis incidunt placeat. Eveniet distinctio est illo sed, id consequuntur iure molestiae rerum assumenda velit inventore voluptatum qui veritatis excepturi.</p>
+    <div className="App-Home-Row">
+      {products.map((product) => (
+        <div>
+          <img src={product.imgUrl} alt={product.name} />
+          <h2>{product.name}</h2>
+          <p>{product.desc}</p>
+          <h4>{product.price}</h4>
+          <p><button>Add to Cart</button></p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
